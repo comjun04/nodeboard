1e4bfc581546f8f38e005600296e4a29d550d5b1
My Name <comjun04@gmail.com>
2021-04-01 14:20:03 +0900

    fix(db): apply multi-db logic to get method

--- src/database/article/get.js
+++ src/database/article/get.js
@@ -2,5 +2,8 @@ module.exports = async (db, data = {}) => {
   const id = parseInt(data.id)
   if (isNaN(id) || id < 0) throw new TypeError('id should be positive integer or 0')
 
-  return db.obj.articles[id]
+  switch (db.type) {
+    case 'internal':
+      return db.obj.articles[id]
+  }
 }
